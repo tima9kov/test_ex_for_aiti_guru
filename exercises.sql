94471d9e7aa828bda12ed202fc1e70d4226ae3da
Тимофей Девятков <tima9kov@gmail.com>
2025-10-28 14:14:14 +0300

    correct 2.1.2

--- exercises.sql
+++ exercises.sql
@@ -13,38 +13,27 @@ ORDER BY total_amount DESC;
 
 --2.1. Найти количество дочерних элементов первого уровня вложенности для категорий номенклатуры.
 
--- Вариант 1.
-SELECT
-    parent.name AS category_name,
-    COUNT(child.id) AS children_count
-FROM categories parent
-LEFT JOIN categories child ON child.parent_id = parent.id
-GROUP BY parent.id, parent.name
-ORDER BY parent.name;
-
--- Вариант 2.
-
-WITH RECURSIVE category_tree AS (
+WITH RECURSIVE all_categories AS (
     SELECT 
         id,
         name,
         parent_id,
         0 as level
     FROM categories 
+    WHERE parent_id IS NULL
     
     UNION ALL
-    
+
     SELECT 
         c.id,
         c.name,
         c.parent_id,
-        ct.level + 1
+        ac.level + 1
     FROM categories c
-    JOIN category_tree ct ON c.parent_id = ct.id
+    JOIN all_categories ac ON c.parent_id = ac.id
 )
 SELECT 
     name as category_name,
-    (SELECT COUNT(*) FROM categories WHERE parent_id = category_tree.id) as children_count
-FROM category_tree
-WHERE level = 0
-ORDER BY name;
+    (SELECT COUNT(*) FROM categories WHERE parent_id = all_categories.id) as children_count
+FROM all_categories
+ORDER BY level, name;
